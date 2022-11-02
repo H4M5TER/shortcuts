70490e0d85824e48d86b21bbef2813861c7b33e8
H4M5TER <zhuxx2003@gmail.com>
2022-11-03 07:28:03 +0800

    20221103

--- Programs/Startup/WSL.ps1
+++
@@ -1,5 +0,0 @@
-$WSL_HOST = bash.exe -c "ifconfig eth0 | awk '/inet / {print \`$2}'"
-
-# Code Server
-netsh interface portproxy delete v4tov4 listenport=8080 listenaddress=0.0.0.0 protocol=tcp
-netsh interface portproxy add v4tov4 listenport=8080 listenaddress=0.0.0.0 connectport=8080 connectaddress=$WSL_HOST protocol=tcp
